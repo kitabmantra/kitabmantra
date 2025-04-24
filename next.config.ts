b31166f248db63f551f8870171e8b619a5c00412
kitabmantra <kitabmantra08@gmail.com>
2025-04-24 07:56:10 +0545

    6th commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+// next.config.mjs
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,      
+  swcMinify: true,            
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",         
+  register: true,         
+  skipWaiting: true,  
+})(nextConfig);
