eab5bf5a717ec193f50b1163adba3b97c553a603
kitabmantra <kitabmantra08@gmail.com>
2025-04-24 08:23:14 +0545

    added vercel analytics

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-// next.config.mjs
-import withPWA from 'next-pwa';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,      
-  swcMinify: true,            
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-export default withPWA({
-  dest: "public",         
-  register: true,         
-  skipWaiting: true,  
-})(nextConfig);
+export default nextConfig;
